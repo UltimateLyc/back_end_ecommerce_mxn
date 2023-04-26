2f88f285e2f436adad1cd4b64ee3739ac6091dbf
Rafael Ramos <ingrnrg@gmail.com>
2023-04-26 01:11:02 -0600

    feat: Created database.js whit sequelize

---
+++ src/database/database.js
@@ -0,0 +1,15 @@
+import { Sequelize } from 'sequelize'
+import dotenv from 'dotenv'
+
+dotenv.config({ path: '.env' })
+// Inicio de la ORM
+export const sequelize = new Sequelize(
+  process.env.DBNAME,
+  process.env.DBUSER,
+  process.env.DBPASSWORD,
+  {
+    host: process.env.DBHOST,
+    port: process.env.DBPORT,
+    dialect: 'postgres'
+  }
+)
