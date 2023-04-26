d0d37b30faeccd8db0965ce7c8ac7345bafb3714
Rafael Ramos <ingrnrg@gmail.com>
2023-04-26 01:19:00 -0600

    feat: Created connection whit the DB with sequelize

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 
 import app from './app.js'
-// import dotenv from 'dotenv'
+import dotenv from 'dotenv'
+import { sequelize } from './database/database.js'
 
 async function main () {
   try {
-    // dotenv.config({ path: '.env' }) // Configuramos el .env con dotenv
+    dotenv.config({ path: '.env' }) // Configuramos el .env con dotenv
+    await sequelize.sync({ force: false }) // Sincroniza el codigo con lo que hara en la base de datos // force:false sirve para que no se modifique la tabla ya creada
+    await sequelize.authenticate() // Verifica que nos podamos conectar a la bd
+    console.log('Connection with the server successful')
     app.listen(process.env.PORT) // Decimos que app escuche el puerto seleccionado
-    console.log('Server ON')
+    console.log(`Server connection on port ${process.env.PORT}`)
   } catch (error) {
     console.log(error)
   }
