5892b3dba313601497d1b28bffa40afa2b0be892
Rafael Ramos <ingrnrg@gmail.com>
2023-04-26 21:22:21 -0600

    feat: added env for sequelize force sync

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { sequelize } from './database/database.js'
 async function main () {
   try {
     dotenv.config({ path: '.env' }) // Configuramos el .env con dotenv
-    await sequelize.sync({ force: false }) // Sincroniza el codigo con lo que hara en la base de datos // force:false sirve para que no se modifique la tabla ya creada
+    await sequelize.sync({ force: process.env.FORCE }) // Sincroniza el codigo con lo que hara en la base de datos // force:false sirve para que no se modifique la tabla ya creada
     await sequelize.authenticate() // Verifica que nos podamos conectar a la bd
     console.log('Connection with the server successful')
     app.listen(process.env.PORT) // Decimos que app escuche el puerto seleccionado
