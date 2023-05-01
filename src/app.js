720dace2f6477c40ed1099dd58aa25f3191c8899
Rafael Ramos <ingrnrg@gmail.com>
2023-05-01 17:37:00 -0600

    feat: Added middleware for error & auth whit jwt

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 import express from 'express'
 import { roleRoutes, userRoutes } from './config/routes.config.js'
+import errorHandler from './middleware/errorMiddleware.js'
 
 const app = express() // manda llamar a ejecutar express para crear el server
 app.use(express.urlencoded({ extends: false })) // Para resibir informacion por body urlencoded
 app.use(express.json()) // Nos permite recibir informacion atravez de archivos JSON
+
+app.use(errorHandler)
 
 app.use(roleRoutes)
 app.use(userRoutes)
